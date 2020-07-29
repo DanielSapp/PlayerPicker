85ab8a2c9ee196911fad13e3d99500299ef0fc72
Daniel Sapp <enipony@gmail.com>
2020-07-29 14:52:12 -0500

    Removed Scanner from Main since user input is now handled by UserInterface.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class Main {
-    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
         int maxCost = ui.getMaxCost();
